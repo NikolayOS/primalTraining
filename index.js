e3c9d40de0588ec274c15a60d31b2949e8acd495
Mykola Osadchii <nikolayosadchii842@gmail.com>
2026-01-23 17:27:55 +0100

    reworked css

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const burger = document.querySelector('.navigation');
 const mobileNav = document.querySelector('.mobile-menu');
+let check = false;
 
 mobileNav.addEventListener('click', () => {
 	burger.classList.toggle('active');
 	mobileNav.classList.toggle('active');
+	if (check) {
+		document.body.style.overflow = 'auto';
+		check = false;
+	} else {
+		document.body.style.overflow = 'hidden';
+		check = true;
+	}
+	console.log(check);
 });
